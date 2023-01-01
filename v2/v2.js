72731756ac73c606947a1d303c00ed31e0b0e2f6
Jonas Majauskas <jonasmajauskas1@gmail.com>
2023-01-01 16:51:19 -0500

    v3

---
+++ v2/v2.js
@@ -0,0 +1,21 @@
+let img = new Image();
+img.src = "/images/chipmunk.jpg";
+document.getElementById('game-board').appendChild(img)
+
+const img = document.createElement("img");
+img.src = "https://picsum.photos/200/301";
+document.body.appendChild(img);
+
+
+
+function GFG_Fun() {
+    var img = new Image();
+    img.src =
+'https://media.geeksforgeeks.org/wp-content/uploads/20190529122828/bs21.png';
+    document.getElementById('body').appendChild(img);
+    down.innerHTML = "Image Element Added.";
+}
+
+let player1image = document.createElement('img');
+player1image.src = "/images/chipmunk.jpg";
+document.getElementById('body').appendChild(player1image);
---
+++ v3/v3.js
@@ -0,0 +1,10 @@
+let chipmunkplayer = document.createElement("img");
+chipmunkplayer.src = "/images/chipmunk.jpg";
+document.body.appendChild(chipmunkplayer);
+
+chipmunkplayer.style.height = "60px";
+chipmunkplayer.style.width = "60px";
+
+function player1start ()
+let ar el = document.getElementById("yabanner");
+chipmunkplayer.innerHTML="<img src=\"http://placehold.it/350x350\" width=\"400px\" height=\"150px\">";
